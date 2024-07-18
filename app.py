c65dadb02fbd7288aaed97792c2967233d4e2663
Radhitya13 <119849371+Radhitya13@users.noreply.github.com>
2024-07-18 22:55:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Train model
 model = LinearRegression()
 model.fit(X_train, y_train)
-
+return df
 # Predict
 y_pred = model.predict(X_test)
 
